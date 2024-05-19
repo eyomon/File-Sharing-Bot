35679660b3371d0ba8b979888cb5316ec4dcb7de
eyomon <148034110+eyomon@users.noreply.github.com>
2024-05-19 19:29:36 +0300

    Username_Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,7 @@
 
 
 import os
+import re
 import logging
 from logging.handlers import RotatingFileHandler
 
@@ -32,9 +33,12 @@ DB_URI = os.environ.get("DATABASE_URL", "")
 DB_NAME = os.environ.get("DATABASE_NAME", "filesharexbot")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
+fsub = os.environ.get("FORCE_SUB_CHANNEL", "0")
 
-TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "100"))
+if bool(re.match(r"^[-+]?\d+(\.\d+)?$", fsub)):
+    FORCE_SUB_CHANNEL = int(fsub) 
+
+TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #start message
 START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link.")
